f147787015762e079c5ce51f14e28b4422026205
unknown <raraph84@gmail.com>
2022-03-06 11:08:52 +0100

    Finish HttpServer

--- src/HttpServer.js
+++ src/HttpServer.js
@@ -1,8 +1,10 @@
 const EventEmitter = require("events");
+const Http = require("http");
 const Request = require("./Request");
 
 module.exports = class HttpServer extends EventEmitter {
 
+    /** @type {Http.Server} */
     #server = null;
 
     constructor() {
@@ -18,6 +20,19 @@ module.exports = class HttpServer extends EventEmitter {
 
                 this.emit("rawRequest", req, res, data);
                 this.emit("request", new Request(req, res, data));
+            });
+        });
+    }
+
+    /**
+     * @param {Number} port 
+     * @returns {Promise} 
+     */
+    listen(port) {
+        return new Promise((resolve, reject) => {
+            this.#server.listen(port, () => {
+                resolve();
+                this.emit("listening");
             });
         });
     }
