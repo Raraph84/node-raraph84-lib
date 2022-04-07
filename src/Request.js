e47e846609f444ab92771987da339f47346dc803
unknown <raraph84@gmail.com>
2022-04-07 18:34:23 +0200

    Fix Request#ip

--- src/Request.js
+++ src/Request.js
@@ -14,7 +14,7 @@ class Request {
         this.data = data;
         this.method = req.method.toUpperCase();
         this.headers = req.headers;
-        this.ip = req.headers["x-forwarded-for"] || "127.0.0.1";
+        this.ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress.slice(7);
 
         const urlSplitted = req.url.split("?");
         this.url = urlSplitted.shift();
