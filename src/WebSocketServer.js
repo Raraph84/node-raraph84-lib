8ccb5ab1ad750ef44ac118f4f40f824078e94443
Raraph84 <contact@raraph.fr>
2022-06-07 19:54:02 +0200

    Fix for autocompletion

--- src/WebSocketServer.js
+++ src/WebSocketServer.js
@@ -11,6 +11,7 @@ module.exports = class WebSocketServer extends EventEmitter {
 
         super();
 
+        /** @type {import("./WebSocketClient")[]} */
         this.clients = [];
 
         this.#server = new WebSocket.Server({ port });
