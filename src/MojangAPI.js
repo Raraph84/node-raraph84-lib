a94708efa602c191e69668e97fe8d542e80e833e
Raraph84 <contact@raraph.fr>
2022-08-04 18:33:31 +0200

    Fix Mojang API

--- src/MojangAPI.js
+++ src/MojangAPI.js
@@ -7,7 +7,7 @@ module.exports = class MojangAPI {
         const req = await fetch("https://api.mojang.com/users/profiles/minecraft/" + playerName);
 
         if (req.status === 204)
-            throw "This user doesn't exist";
+            throw "This user does not exist";
 
         const user = await req.json();
 
@@ -18,8 +18,8 @@ module.exports = class MojangAPI {
 
         const req = await fetch("https://sessionserver.mojang.com/session/minecraft/profile/" + playerUuid + (unsigned ? "?unsigned" : ""));
 
-        if (req.status === 400)
-            throw "This user doesn't exist";
+        if (req.status === 204)
+            throw "This user does not exist";
 
         const user = await req.json();
 
